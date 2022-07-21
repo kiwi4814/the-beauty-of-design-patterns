d381d02b6778e1a89e3485aa44ae56e2fd289468
kiwi4814 <m4814@qq.com>
2022-07-21 08:06:43 +0800

    Local class and test

---
+++ src/main/java/com/geek/java8/classes/Test.java
@@ -0,0 +1,12 @@
+package com.geek.java8.classes;
+
+public class Test {
+    public static void main(String[] args) {
+        new Object() {
+            public void test() {
+                System.out.println(this.getClass().getDeclaringClass()); //null
+                System.out.println(this.getClass().getEnclosingClass()); //not null
+            }
+        }.test();
+    }
+}
